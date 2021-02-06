a44aad091bdca6a4e1d163cfe771c8996be885e1
Warja R <neslob.wr@gmail.com>
2021-02-06 14:56:16 +0100

    Pitch is done

--- Assets/SynthScript.cs
+++ Assets/SynthScript.cs
@@ -7,37 +7,47 @@ public class SynthScript : MonoBehaviour
 {
     [Header("OSC Settings")] public OSCReceiver receiver;
     public OSCTransmitter transmitter;
-
+    
+    //ADRESS
     private const string oscAddressVolume = "/volume";
-    float s_VolumeRatio = 0.0f;
+    private const string oscAddressPitch = "/pitch";
+    
+    //STARTING FLOATS
+    private float s_VolumeRatio = 0.0f;
+    private float s_PitchRatio = 0.0f;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        //VOLUME Transmitter
-       // var messageVol = new OSCMessage(oscAddressVolume);
-      //  messageVol.AddValue(OSCValue.Float(s_VolumeRatio));
-       // transmitter.Send(messageVol);
-       // Debug.Log(messageVol);
-        
     }
     public void SynthVolumeChanged(DialInteractable dial)
     {
         var messageVol = new OSCMessage(oscAddressVolume);
         messageVol.AddValue(OSCValue.Float(s_VolumeRatio));
-        float ratio = dial.CurrentAngle / dial.RotationAngleMaximum;
-        s_VolumeRatio = ratio;
+        float ratioVolume = dial.CurrentAngle / dial.RotationAngleMaximum;
+        s_VolumeRatio = ratioVolume;
         transmitter.Send(messageVol);
+        
         Debug.Log(s_VolumeRatio);
         Debug.Log(messageVol);
-        ;
+        
         
     }
-
-    
-
-    // Update is called once per frame
+     public void SynthPitchChanged(DialInteractable dial)
+        {
+            var messagePitch = new OSCMessage(oscAddressPitch);
+            messagePitch.AddValue(OSCValue.Float(s_PitchRatio));
+            float ratioPitch = dial.CurrentAngle / dial.RotationAngleMaximum;
+            s_PitchRatio = ratioPitch;
+            transmitter.Send(messagePitch);
+            
+            Debug.Log(s_PitchRatio);
+            Debug.Log(messagePitch);
+            ;
+            
+        }
+     // Update is called once per frame
     void Update()
     {
         
