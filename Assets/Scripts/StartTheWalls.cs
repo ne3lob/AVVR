15fc16defca3a8a9f943abbb266576b70572b8dd
Warja R <neslob.wr@gmail.com>
2021-04-08 15:08:13 +0200

    Add animation for the colomn Vfx and Up/down Model

--- Assets/Scripts/StartTheWalls.cs
+++ Assets/Scripts/StartTheWalls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Synth;
@@ -7,9 +8,10 @@ using UnityEngine.VFX;
 
 public class StartTheWalls : MonoBehaviour
 {
-    [SerializeField] public GameObject wallsVfx;
-    public VisualEffect myEffect;
-    public static readonly string SPAWN_RATE_NAME = "SizeRate";
+    public GameObject colomnReally;
+    public VisualEffect[] myEffects;
+    public static readonly string SPAWN_RATE_NAME_A = "CapacityA";
+    public static readonly string SPAWN_RATE_NAME_B = "CapacityB";
 
     public MeshRenderer wallsObj;
 
@@ -27,17 +29,22 @@ public class StartTheWalls : MonoBehaviour
     private float lerpDuration = 1000.0f;
     private bool lerpNow = false;
     private float lerpStart = 0;
+    private bool lerpNowSecond;
+    private float lerpStartSecond;
 
     private SynthScript _synthScript;
     private float pitchValue;
     public GameObject synthObj;
 
     private bool one = false;
+    private bool two = false;
 
-    float currentDiss=0.93f;
-    float targetDissNull=0f;
-    float targetDissOne=0.93f;
-    
+
+    float currentDiss = 0.93f;
+    float targetDissNull = 0f;
+    float targetDissOne = 0.93f;
+    private float speed = 0.5f;
+
 
     private const string _wallsShaderProp = "_Blend";
     private const string _wallsRestProp = "_Blend2";
@@ -45,23 +52,36 @@ public class StartTheWalls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        wallsVfx.SetActive(false);
+        //  wallsVfx.SetActive(false);
         _synthScript = synthObj.GetComponent<SynthScript>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         pitchValue = _synthScript.s_PitchRatio;
+        float step = speed * Time.deltaTime;
 
         if (!one && pitchValue >= 0.1f)
         {
-            wallsVfx.SetActive(true);
-
             lerpNow = true;
             lerpStart = Time.time;
             one = true;
+        }
+
+        if (!two && pitchValue >= 0.2f)
+        {
+            lerpNowSecond = true;
+            lerpStartSecond = Time.time;
+            two = true;
+        }
+
+        if (lerpNowSecond)
+        {
+            ColomnVfxMax();
+
+            colomnReally.transform.position =
+                Vector3.MoveTowards(colomnReally.transform.position, new Vector3(0, -5.65f, 0.08f), step);
         }
 
         if (lerpNow)
@@ -78,6 +98,23 @@ public class StartTheWalls : MonoBehaviour
             lerpNow = false;
             lerpStart = Time.time;
         }
+
+        if (two && pitchValue <= 0.2)
+        {
+            two = false;
+            lerpNowSecond = false;
+            lerpStartSecond = Time.time;
+        }
+
+        if (!lerpNowSecond)
+
+        {
+            ColomnVfxNull();
+            colomnReally.transform.position =
+                Vector3.MoveTowards(colomnReally.transform.position, new Vector3(0, -0.3f, 0.08f), step);
+        }
+
+
         if (!lerpNow)
 
         {
@@ -92,31 +129,62 @@ public class StartTheWalls : MonoBehaviour
         var progress = Time.time - lerpStart;
 
         currentDiss = Mathf.Lerp(currentDiss, targetDissNull, progress / lerpDuration);
-        
+
         obj.material.SetFloat(prop, currentDiss);
 
-        myEffect.SetFloat(SPAWN_RATE_NAME, 4000);
-
-        if (lerpDuration < progress)
-        {
-            lerpNow = false;
-        }
+        // myEffect.SetFloat(SPAWN_RATE_NAME, 4000);
+        //
+        // if (lerpDuration < progress)
+        // {
+        //     lerpNow = false;
+        // }
     }
+
     void ChangeToNormalStationLerp(MeshRenderer obj, string prop)
     {
         var progress = Time.time - lerpStart;
 
         currentDiss = Mathf.Lerp(currentDiss, targetDissOne, progress / lerpDuration);
-        
+
         obj.material.SetFloat(prop, currentDiss);
 
-        myEffect.SetFloat(SPAWN_RATE_NAME, 0);
+        // myEffect.SetFloat(SPAWN_RATE_NAME, 0);
+        //
+        // if (lerpDuration < progress)
+        // {
+        //     lerpNow = false;
+        // }
+    }
 
-        if (lerpDuration < progress)
+    void ColomnVfxNull()
+    {
+        var progressVfx = Time.time - lerpStartSecond;
+
+        foreach (var effect in myEffects)
         {
-            lerpNow = false;
+            effect.SetFloat(SPAWN_RATE_NAME_A, 0);
+            effect.SetFloat(SPAWN_RATE_NAME_B, 0);
+        }
+
+        if (lerpDuration < progressVfx)
+        {
+            lerpNowSecond = false;
         }
     }
-    
-    
+
+    void ColomnVfxMax()
+    {
+        var progressVfx = Time.time - lerpStartSecond;
+
+        foreach (var effect in myEffects)
+        {
+            effect.SetFloat(SPAWN_RATE_NAME_A, 20);
+            effect.SetFloat(SPAWN_RATE_NAME_B, 30);
+        }
+
+        if (lerpDuration < progressVfx)
+        {
+            lerpNowSecond = false;
+        }
+    }
 }
