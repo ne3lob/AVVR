6292528c52efbd0678c8bccb5c361a3e1f5e7170
Warja R <neslob.wr@gmail.com>
2021-04-10 01:42:32 +0200

    add first animation for the circles!! need to add new animations for the Circles endings

--- Assets/Scripts/StartTheWalls.cs
+++ Assets/Scripts/StartTheWalls.cs
@@ -8,36 +8,51 @@ using UnityEngine.VFX;
 
 public class StartTheWalls : MonoBehaviour
 {
+    //PUBLIC
+
     public GameObject colomnReally;
     public VisualEffect[] myEffects;
-    public static readonly string SPAWN_RATE_NAME_A = "CapacityA";
-    public static readonly string SPAWN_RATE_NAME_B = "CapacityB";
 
-    public MeshRenderer wallsObj;
-
+    [Header("WallsMeshRender")] public MeshRenderer wallsObj;
     public MeshRenderer blackWallsObj;
-
     public MeshRenderer bottomCellingObj;
 
+    [Header("MaterialsCircles")] public Material FirstCircleEnvironment;
+    public Material SecondCircleEnvironment;
 
     private Material newMatWall;
     private Material newMatBlackWall;
     private Material newMatCelling;
 
+    public GameObject synthObj;
+
+
+    //PRIVATE 
+
     private Color newTranspWall;
 
     private float lerpDuration = 1000.0f;
-    private bool lerpNow = false;
+    private bool lerpNow;
     private float lerpStart = 0;
     private bool lerpNowSecond;
     private float lerpStartSecond;
 
+    private bool courOn = true;
+
+
     private SynthScript _synthScript;
     private float pitchValue;
-    public GameObject synthObj;
 
-    private bool one = false;
-    private bool two = false;
+
+    private bool one;
+    private bool two;
+
+
+    private float firstCurrentStrate = 13f;
+    private float firstTargetStrate = -24f;
+
+    private float secondCurrentStrate = -24f;
+    private float secondTargetStrate = 13f;
 
 
     float currentDiss = 0.93f;
@@ -45,22 +60,27 @@ public class StartTheWalls : MonoBehaviour
     float targetDissOne = 0.93f;
     private float speed = 0.5f;
 
-
+    private static readonly string SPAWN_RATE_NAME_A = "CapacityA";
+    private static readonly string SPAWN_RATE_NAME_B = "CapacityB";
+    private static readonly int _up = Shader.PropertyToID("Up");
     private const string _wallsShaderProp = "_Blend";
     private const string _wallsRestProp = "_Blend2";
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //  wallsVfx.SetActive(false);
         _synthScript = synthObj.GetComponent<SynthScript>();
+        FirstCircleEnvironment.SetFloat("Up", 13f);
+        SecondCircleEnvironment.SetFloat("Up", -24f);
     }
 
     // Update is called once per frame
     void Update()
     {
         pitchValue = _synthScript.s_PitchRatio;
-        float step = speed * Time.deltaTime;
+        float stepSpeed = speed * Time.deltaTime;
+
 
         if (!one && pitchValue >= 0.1f)
         {
@@ -74,18 +94,23 @@ public class StartTheWalls : MonoBehaviour
             lerpNowSecond = true;
             lerpStartSecond = Time.time;
             two = true;
+            courOn = true;
         }
 
         if (lerpNowSecond)
         {
             ColomnVfxMax();
-
             colomnReally.transform.position =
-                Vector3.MoveTowards(colomnReally.transform.position, new Vector3(0, -5.65f, 0.08f), step);
-            if (colomnReally.transform.position == new Vector3(0, -2f, 0.08f))
+                Vector3.MoveTowards(colomnReally.transform.position, new Vector3(0, -5.65f, 0.08f), stepSpeed);
+            //SECOND CIRCLE
+            if (courOn)
             {
-                
-                Debug.Log("Rabotaet");
+                var newProgress = Time.time - lerpStart;
+                firstCurrentStrate = Mathf.Lerp(firstCurrentStrate, firstTargetStrate, newProgress / lerpDuration);
+                StartCoroutine(ColomnFallFirstSecondCircle(FirstCircleEnvironment, firstCurrentStrate, 5));
+
+                secondCurrentStrate = Mathf.Lerp(secondCurrentStrate, secondTargetStrate, newProgress / lerpDuration);
+                StartCoroutine(ColomnFallFirstSecondCircle(SecondCircleEnvironment, secondCurrentStrate, 10));
             }
         }
 
@@ -109,6 +134,7 @@ public class StartTheWalls : MonoBehaviour
             two = false;
             lerpNowSecond = false;
             lerpStartSecond = Time.time;
+            courOn = false;
         }
 
         if (!lerpNowSecond)
@@ -116,7 +142,19 @@ public class StartTheWalls : MonoBehaviour
         {
             ColomnVfxNull();
             colomnReally.transform.position =
-                Vector3.MoveTowards(colomnReally.transform.position, new Vector3(0, -0.3f, 0.08f), step);
+                Vector3.MoveTowards(colomnReally.transform.position, new Vector3(0, -0.3f, 0.08f), stepSpeed);
+            //SECOND CIRCLE
+            if (!courOn)
+            {
+                //TODO
+                
+                var newProgressSecond = Time.time - lerpStart;
+                firstTargetStrate = Mathf.Lerp(firstTargetStrate, firstCurrentStrate, newProgressSecond / lerpDuration);
+                StartCoroutine(ColomnFallFirstSecondCircle(FirstCircleEnvironment, firstTargetStrate, 5));
+                Debug.Log(firstTargetStrate);
+                
+                // StartCoroutine(ColomnFallFirstSecondCircle(FirstCircleEnvironment, 10f, true, 8));
+            }
         }
 
 
@@ -129,6 +167,14 @@ public class StartTheWalls : MonoBehaviour
         }
     }
 
+    IEnumerator ColomnFallFirstSecondCircle(Material circle, float valueUp,  int delay_)
+    {
+        yield return new WaitForSeconds(delay_);
+        circle.SetFloat("Up", valueUp);
+        
+    }
+
+
     void ChangeToTransparentLerp(MeshRenderer obj, string prop)
     {
         var progress = Time.time - lerpStart;
@@ -136,14 +182,8 @@ public class StartTheWalls : MonoBehaviour
         currentDiss = Mathf.Lerp(currentDiss, targetDissNull, progress / lerpDuration);
 
         obj.material.SetFloat(prop, currentDiss);
-
-        // myEffect.SetFloat(SPAWN_RATE_NAME, 4000);
-        //
-        // if (lerpDuration < progress)
-        // {
-        //     lerpNow = false;
-        // }
     }
+
 
     void ChangeToNormalStationLerp(MeshRenderer obj, string prop)
     {
@@ -152,13 +192,6 @@ public class StartTheWalls : MonoBehaviour
         currentDiss = Mathf.Lerp(currentDiss, targetDissOne, progress / lerpDuration);
 
         obj.material.SetFloat(prop, currentDiss);
-
-        // myEffect.SetFloat(SPAWN_RATE_NAME, 0);
-        //
-        // if (lerpDuration < progress)
-        // {
-        //     lerpNow = false;
-        // }
     }
 
     void ColomnVfxNull()
